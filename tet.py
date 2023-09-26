75341b832fbe90549d850fd8e91b20eb85c810aa
shivapranav8 <72888886+shivapranav8@users.noreply.github.com>
2023-09-27 01:30:21 +0530

    tet.py

--- tet.py
+++ tet.py
@@ -1,19 +1,11 @@
 import requests
 
 def test_hello_endpoint():
-    # Define the base URL of your FastAPI app
+
     base_url = "http://localhost:8000"
-
-    # Send a GET request to the /hello endpoint
     response = requests.get(f"{base_url}/hello")
-
-    # Check if the response status code is 200 (OK)
     assert response.status_code == 200
-
-    # Parse the JSON response
     data = response.json()
-
-    # Check if the response message is "hi Shiva"
     assert data["message"] == "hi Shiva"
 
 if __name__ == "__main__":
